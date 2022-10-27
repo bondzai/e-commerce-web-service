f54da4f6de151779620384c9191c904817184c27
introbond <introbond.dev@gmail.com>
2022-10-27 14:03:08 +0700

    basic structure & config

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoccumaent = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoccumaent));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use(morgan('tiny'));
+
+
+const home = require('./route/home');
+app.use('/api/v1', home);
 
 module.exports = app;
