8306b61c0a141674ccf6be3c42f1108dc205a34c
introbond <introbond.dev@gmail.com>
2022-10-27 21:20:18 +0700

    signup done with httpOnly cookie

--- util/cookieToken.js
+++ util/cookieToken.js
@@ -4,8 +4,9 @@ const cookieToken = (user, res) => {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRY * 24 * 60 * 60 * 1000),
         httpOnly: true,
     };
+    user.password = undefined;
     res.status(200)
-        .cookie('token', token.options)
+        .cookie('token', token, options)
         .json({
             success: true,
             token,
