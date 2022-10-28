9f89dcef6414fc3a313eff37f0cb2622b6b4ef28
introbond <introbond.dev@gmail.com>
2022-10-28 10:31:26 +0700

    done: signup route

--- controller/userController.js
+++ controller/userController.js
@@ -2,27 +2,22 @@ const User = require('../model/user');
 const BigPromise = require('../middleware/BigPromise');
 const CustomError = require('../util/customError');
 const cookieToken = require('../util/cookieToken');
-const fileUpload = require('express-fileupload');
 const cloudinary = require('cloudinary');
 
 exports.signup = BigPromise(async (req, res, next) => {
-    
-    let result;
-    if (req.files) {
-        let file = req.files.photo;
-        result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
-            folder: "users",
-            width: 150,
-            crop: "scale"
-        });
-    };
-
+    if (!req.files) {
+        return next (new CustomError('pho to is required to signup',400));
+    }
     const {name, email, password} = req.body;
-
     if (!email || !name || !password) {
         return next(new CustomError('name, email & password are required', 400));
     };
-
+    let file = req.files.photo;
+    const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+        folder: "users",
+        width: 150,
+        crop: "scale"
+    });
     const user = await User.create({
         name,
         email,
@@ -32,6 +27,5 @@ exports.signup = BigPromise(async (req, res, next) => {
             secure_url: result.secure_url
         }
     })
-
     cookieToken(user, res);
 });
