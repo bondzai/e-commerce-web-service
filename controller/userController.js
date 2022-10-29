8f09a48b07b2ef3e964a9a531f6d8408b83a7fb8
introbond <introbond.dev@gmail.com>
2022-10-29 12:36:51 +0700

    done: forgot password route

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,7 @@ const BigPromise = require('../middleware/BigPromise');
 const CustomError = require('../util/customError');
 const cookieToken = require('../util/cookieToken');
 const cloudinary = require('cloudinary');
+const mailHelper = require('../util/emailHelper');
 
 exports.signup = BigPromise(async (req, res, next) => {
     if (!req.files) {
@@ -51,12 +52,41 @@ exports.login = BigPromise(async (req, res, next) => {
 });
 
 exports.logout = BigPromise(async (req, res, next) => {
-res.cookie("token", null, {
-    expires: new Date(Date.now()),
-    httpOnly: true,
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+    res.status(200).json({
+        succes: true,
+        message: "Logout success",
+    });
 });
-res.status(200).json({
-    succes: true,
-    message: "Logout success",
+
+exports.forgotPassword = BigPromise(async (req, res, next) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return next(new CustomError("Email not found as registered", 400));
+    };
+    const forgotToken = user.getForgotPasswordToken();
+    await user.save({ validateBeforeSave: false });
+    const myUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${forgotToken}`;
+    const message = `Copy paste this link in your URL and hit enter \n\n ${myUrl}`;
+    try {
+        await mailHelper({
+            email: user.email,
+            subject: "JB eCommerce - Password reset email",
+            message,
+        });
+        res.status(200).json({
+        succes: true,
+        message: "Email sent successfully",
+        });
+    } catch (error) {
+        user.forgotPasswordToken = undefined;
+        user.forgotPasswordExpiry = undefined;
+        await user.save({ validateBeforeSave: false });
+        return next(new CustomError(error.message, 500));
+    }
 });
-});
+
